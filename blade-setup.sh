a045ff960f5f077b61054179a34f2f6267800b76
Jerry Belton <jerry.belton@gmail.com>
2016-05-28 19:47:16 -0700

    Update blade-setup.sh

--- blade-setup.sh
+++ blade-setup.sh
@@ -7,3 +7,9 @@ sudo modprobe ath10k_pci
 
 #glxinfo
 sudo apt-get install mesa-utils
+
+#glxinfo and lspci will fail until kernel gets updated
+wget http://kernel.ubuntu.com/%7Ekernel-ppa/mainline/v4.6-rc7-wily/linux-headers-4.6.0-040600rc7-generic_4.6.0-040600rc7.201605081830_amd64.deb
+wget http://kernel.ubuntu.com/%7Ekernel-ppa/mainline/v4.6-rc7-wily/linux-headers-4.6.0-040600rc7_4.6.0-040600rc7.201605081830_all.deb
+wget http://kernel.ubuntu.com/%7Ekernel-ppa/mainline/v4.6-rc7-wily/linux-image-4.6.0-040600rc7-generic_4.6.0-040600rc7.201605081830_amd64.deb
+sudo dpkg -i linux*
