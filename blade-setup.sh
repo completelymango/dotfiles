998c21f223b6139ad6f849b17121d451679c151f
Jerry Belton <jerry.belton@gmail.com>
2016-05-28 19:01:01 -0700

    Update blade-setup.sh

--- blade-setup.sh
+++ blade-setup.sh
@@ -4,3 +4,6 @@
 sudo wget -O /lib/firmware/ath10k/QCA6174/hw3.0/board-2.bin https://github.com/kvalo/ath10k-firmware/blob/master/QCA6174/hw3.0/board-2.bin?raw=true
 sudo rmmod ath10k_pci ath10k_core
 sudo modprobe ath10k_pci
+
+#glxinfo
+sudo apt-get install mesa-utils
